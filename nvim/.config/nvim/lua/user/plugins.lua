7137d59bfefff8402bd584b19cfd2bd17b04a689
Max Sydney <maxsydney8@gmail.com>
2025-10-18 11:23:52 -0700

    Switch to lazy

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,119 +1,120 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system({
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-    })
-    print("Installing packer close and reopen Neovim...")
-    vim.cmd([[packadd packer.nvim]])
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+-- Configure lazy.nvim
+require("lazy").setup({
+    -- Core dependencies
+    { "nvim-lua/plenary.nvim" },
+    { "windwp/nvim-autopairs" },
+    { "numToStr/Comment.nvim" },
+    { "kyazdani42/nvim-web-devicons" },
+    { "kyazdani42/nvim-tree.lua" },
+    { "akinsho/bufferline.nvim" },
+    { "moll/vim-bbye" },
+    { "nvim-lualine/lualine.nvim" },
+    { "ahmedkhalf/project.nvim" },
+    { "lukas-reineke/indent-blankline.nvim" },
+    { "folke/which-key.nvim" },
+    { "ThePrimeagen/harpoon" },
 
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-    return
-end
-
--- Have packer use a popup window
-packer.init({
-    display = {
-        open_fn = function()
-            return require("packer.util").float({ border = "rounded" })
-        end,
-    },
-})
--- Install your plugins here
-return packer.startup(function(use)
-    use { "wbthomason/packer.nvim" } -- Have packer manage itself
-    use { "nvim-lua/plenary.nvim" }  -- Useful lua functions used by lots of plugins
-    use { "windwp/nvim-autopairs" }  -- Autopairs, integrates with both cmp and treesitter
-    use { "numToStr/Comment.nvim" }
-    use { "kyazdani42/nvim-web-devicons" }
-    use { "kyazdani42/nvim-tree.lua" }
-    use { "akinsho/bufferline.nvim" }
-    use { "moll/vim-bbye" }
-    use { "nvim-lualine/lualine.nvim" }
-    use { "ahmedkhalf/project.nvim" }
-    use { "lukas-reineke/indent-blankline.nvim" }
-    use { "folke/which-key.nvim" }
-    use { "ThePrimeagen/harpoon" }
-
-    -- Terminal stuff
-    use { "akinsho/toggleterm.nvim" }
+    -- Terminal
+    { "akinsho/toggleterm.nvim" },
 
     -- Colorschemes
-    use { "lunarvim/Onedarker.nvim" }
-    use { "ellisonleao/gruvbox.nvim" }
-    use { "navarasu/onedark.nvim" }
-    use { "catppuccin/nvim", as = "catppuccin" }
+    { "lunarvim/Onedarker.nvim" },
+    { "ellisonleao/gruvbox.nvim" },
+    { "navarasu/onedark.nvim" },
+    { "catppuccin/nvim", name = "catppuccin" },
 
-    -- cmp plugins
-    use { "hrsh7th/nvim-cmp" }         -- The completion plugin
-    use { "hrsh7th/cmp-buffer" }       -- buffer completions
-    use { "hrsh7th/cmp-path" }         -- path completions
-    use { "saadparwaiz1/cmp_luasnip" } -- snippet completions
-    use { "hrsh7th/cmp-nvim-lsp" }
-    use { "hrsh7th/cmp-nvim-lua" }
+    -- Completion plugins
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-nvim-lua" },
 
-    -- snippets
-    use { "L3MON4D3/LuaSnip", commit = "79b2019c68a2ff5ae4d732d50746c901dd45603a" }             --snippet engine
-    use { "rafamadriz/friendly-snippets", commit = "d27a83a363e61009278b6598703a763ce9c8e617" } -- a bunch of snippets to use
+    -- Snippets
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
+    },
+    { "rafamadriz/friendly-snippets" },
 
     -- LSP
-    use {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-    }
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "neovim/nvim-lspconfig" },
 
     -- Telescope
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = { 'nvim-lua/plenary.nvim' }
-    }
-    use { "nvim-telescope/telescope-live-grep-args.nvim" }
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-    use {
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    { "nvim-telescope/telescope-live-grep-args.nvim" },
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+    },
+    {
         "nvim-telescope/telescope-frecency.nvim",
-        requires = { "kkharji/sqlite.lua" }
-    }
+        dependencies = { "kkharji/sqlite.lua" },
+    },
+
     -- Treesitter
-    use { "nvim-treesitter/nvim-treesitter", run = ':TSUpdate' }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
 
     -- Debugging
-    use { "mfussenegger/nvim-dap" }
-    use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
-    use { "mfussenegger/nvim-dap-python" }
-    use { "theHamsta/nvim-dap-virtual-text" }
-    use { "nvim-neotest/nvim-nio" } -- Used by dap-ui
-    use { 'nvim-telescope/telescope-dap.nvim' }
-    use { 'ldelossa/nvim-dap-projects' }
-    use {
+    { "mfussenegger/nvim-dap" },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    },
+    { "mfussenegger/nvim-dap-python" },
+    { "theHamsta/nvim-dap-virtual-text" },
+    { "nvim-neotest/nvim-nio" },
+    { "nvim-telescope/telescope-dap.nvim" },
+    { "ldelossa/nvim-dap-projects" },
+    {
         "jay-babu/mason-nvim-dap.nvim",
-        requires = {
+        dependencies = {
             "williamboman/mason.nvim",
             "mfussenegger/nvim-dap",
-        }
-    }
+        },
+    },
 
     -- Git
-    use { "sindrets/diffview.nvim" }
+    { "sindrets/diffview.nvim" },
 
     -- Visual tweaks
-    use { 'stevearc/dressing.nvim' }
-end)
+    { "stevearc/dressing.nvim" },
+}, {
+    ui = {
+        border = "rounded",
+    },
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        notify = false,
+    },
+})
