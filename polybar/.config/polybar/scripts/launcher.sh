275fb302a3b6de41b70eaaf43609ca97994f566c
Max Sydney <maxsydney8@gmail.com>
2025-10-18 12:46:03 -0700

    Improve rofi config. Now uses consistent theme and better window switching

--- polybar/.config/polybar/scripts/launcher.sh
+++ polybar/.config/polybar/scripts/launcher.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+## Application Launcher & Window Switcher
+## Combined interface with multiple modes
+
 FILE="$HOME/.config/polybar/scripts/rofi/colors.rasi"
 
-## random accent color
+## random accent color (disabled by default)
 #COLORS=('#EC7875' '#EC6798' '#BE78D1' '#75A4CD' '#00C7DF' '#00B19F' '#61C766' \
 #		'#B9C244' '#EBD369' '#EDB83F' '#E57C46' '#AC8476' '#6C77BB' '#6D8895')
 #AC="${COLORS[$(( $RANDOM % 14 ))]}"
@@ -10,4 +13,6 @@ FILE="$HOME/.config/polybar/scripts/rofi/colors.rasi"
 #sed -i -e "s/ac: .*/ac:   ${AC}FF;/g" $FILE
 #sed -i -e "s/se: .*/se:   ${SE}FF;/g" $FILE
 
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/scripts/rofi/launcher.rasi
+# Launch rofi with both drun and window modes
+# Use Ctrl+Tab or Left/Right arrow keys to switch between modes
+rofi -no-config -no-lazy-grab -show drun -modi drun,window -theme ~/.config/polybar/scripts/rofi/launcher.rasi
---
+++ polybar/.config/polybar/scripts/window-switcher.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+## Window Switcher using rofi
+## Shows all open windows for quick switching
+
+rofi -no-config -no-lazy-grab -show window -theme ~/.config/polybar/scripts/rofi/window-switcher.rasi
